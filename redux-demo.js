ccd19aff6940f869e67bd407079cfa62143a7152
arpit00714 <arpitkhare14@gmail.com>
2023-04-30 14:04:01 +0530

    second commit

--- redux-demo.js
+++ redux-demo.js
@@ -6,6 +6,12 @@ const counterReducer = (state = {counter : 0},action) => {
     if (action.type === "DECREMENT") return {
         counter : state.counter - 1
     }
+    if (action.type === "DECREMENT2") return {
+        counter : state.counter - 2
+    }
+    if (action.type === "INCREMENT2") return {
+        counter : state.counter + 2
+    }
      return {
         counter : state.counter + 1
     }
@@ -26,4 +32,9 @@ store.dispatch({type : 'INCREMENT'})
 store.dispatch({type : 'INCREMENT'})
 store.dispatch({type : 'INCREMENT'})
 store.dispatch({type : 'INCREMENT'})
+
+
+store.dispatch({type : 'INCREMENT2'})
+store.dispatch({type : 'DECREMENT2'})
+
 store.dispatch({type : 'DECREMENT'})
